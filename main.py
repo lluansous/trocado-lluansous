5fa48ab6ab238ca392c027824d4a8cb5bf7f49a5
lluansous <luansousa@protonmail.com>
2021-01-07 17:19:40 +0000

    coloquei uma variavel valor, calculei as condições e fechei com um f string.

--- main.py
+++ main.py
@@ -4,7 +4,26 @@
 def main():
     """Função principal que será rodada quando o script for passado para o interpretador."""
     # COLOQUE SEU CÓDIGO AQUI
-
+    Valor = float(input('Digite um valor entre R$0,00 e R$10,00.'))
+    print('São necessárias:')
+    Valor = Valor*100
+    if Valor // 100 > 0:
+      print(f'moedas de R$ 1.00: {Valor//100:.0f}')
+      Valor = Valor%100 
+    if Valor//50 > 0:
+      print(f'moedas de R$ 0.50: {Valor//50:.0f}')
+      Valor = Valor%50
+    if Valor//25 > 0:
+      print(f'moedas de R$ 0.25: {Valor//25:.0f}')
+      Valor = Valor%25
+    if Valor//10 > 0:
+      print(f'moedas de R$ 0.10: {Valor//10:.0f}')
+      Valor = Valor%10 
+    if Valor//5 > 0:
+      print(f'moedas de R$ 0.05: {Valor//5:.0f}')
+      Valor = Valor%5
+    if Valor//1 > 0:
+      print(f'moedas de R$ 0.01: {Valor//1:.0f}')  
 
 if __name__ == '__main__':
     main()
